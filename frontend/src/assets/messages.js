7b3d68caced37f92ad72295eb54c4f73f30be588
Etienne Mbade NDOUR <ndouretienneoscar@gmail.com>
2025-10-04 18:09:17 +0000

    Updates

--- frontend/src/assets/messages.js
+++
@@ -1,3 +0,0 @@
-// src/assets/messages.js
-export const spiritualMessage = "Que la lumière du Christ illumine chaque page de cette application.";
-export const parishName = "Paroisse Sainte Croix de Bambey";
